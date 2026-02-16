fe8708346371c1efdae4599c7809318aeba5ad75
kanyalharshit30-ship-it <kanyalharshit30@gmail.com>
2026-02-16 20:57:08 +0530

    Add files via upload

---
+++ hkjava.java
@@ -0,0 +1,11 @@
+import java.net.Socket;
+import java.net.SocketPermission;
+import java.util.Scanner; 
+
+public class hkjava{
+    public static void main(String[] args) {
+        int a=12,b=5,c=5,d=4;
+        System.out.println((a*a+b*b)/2*c*d);
+
+    }
+}
