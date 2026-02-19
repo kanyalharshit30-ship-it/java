986916dda2cbe7b663700d89f893e1bcba78dfda
Harshit kanyal <kanyalharshit30@gmail.com>
2026-02-19 19:01:10 +0530

    program to find whether a given no. is integer or not.

---
+++ find_integer.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class find_integer{
+    public static void main(String[] args)
+    {
+        System.out.println("enter no: ");
+        Scanner hk= new Scanner(System.in);
+        System.out.println(hk.hasNextInt());
+    }
+}
+    
